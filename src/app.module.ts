d9d3e414fd8b1e6de76b88dc6b5ca90a25f79bbc
ArrayaWongsaita <152959154+ArrayaWongsaita@users.noreply.github.com>
2026-03-17 15:08:43 +0700

    feat(inventrory):create crud inventrory api (#4)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { CategoriesModule } from './categories/categories.module';
+import { InventoryModule } from './inventory/inventory.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, CategoriesModule],
+  imports: [AuthModule, UsersModule, InventoryModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
