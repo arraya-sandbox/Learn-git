442741d9d367d08e642011beaefb4db0d0d99da3
ArrayaWongsaita <152959154+ArrayaWongsaita@users.noreply.github.com>
2026-03-19 14:21:35 +0700

    Feature/addresses resource (#5)
    * feat(addresses): creaet resource
    
    * chore(addresses): delete file test

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { CategoriesModule } from './categories/categories.module';
 import { InventoryModule } from './inventory/inventory.module';
+import { AddressesModule } from './addresses/addresses.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, InventoryModule, CategoriesModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    InventoryModule,
+    CategoriesModule,
+    AddressesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
